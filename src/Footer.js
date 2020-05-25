92accf88dec27223a576973b4e8a24738a0925cb
Gant Laborde <gantman@gmail.com>
2020-05-25 13:18:06 -0500

    add footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-  return <div>I am Footer</div>
+  return (
+    <div class="footer">
+      <div>
+        <p>Copyright (Now) Infinite Red</p>
+      </div>
+
+      <img
+        className="footerImage"
+        src="./masks.png"
+        alt="comedy and tragedy masks footer"
+      />
+      <div>
+        <p>Created by Gant Laborde</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
