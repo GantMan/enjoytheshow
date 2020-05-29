5920d46055516b1eee0e5e9a56fd549652cf6ed8
Gant Laborde <gantman@gmail.com>
2020-05-29 16:06:42 -0500

    beautiful live reporting

--- src/WatchRoom.js
+++ src/WatchRoom.js
@@ -1,35 +1,86 @@
-import React, { useReducer, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import API from "@aws-amplify/api";
-import { onUpdateByRoomId } from "./graphql/subscriptions";
-import { itemsByRoomName } from "./graphql/queries";
-import { VictoryPie, VictoryTooltip } from "victory";
+import React, { useReducer, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import API from '@aws-amplify/api'
+import { onUpdateByRoomId } from './graphql/subscriptions'
+import { itemsByRoomName } from './graphql/queries'
+import { VictoryPie, VictoryTooltip } from 'victory'
+
+// const demoChart = [
+//   { x: 'Happy', y: 1 },
+//   { x: 'Neutral', y: 8 },
+//   { x: 'Surprised', y: 14 },
+//   { x: 'Sad', y: 1 },
+//   { x: 'Fearful', y: 1 },
+//   { x: 'Disgusted', y: 11 },
+//   { x: 'Angry', y: 1 },
+// ]
+
+class CustomLabel extends React.Component {
+  static defaultEvents = VictoryTooltip.defaultEvents
+  render() {
+    const { x, y, datum } = this.props
+    const cat = datum.y > 0 ? datum.x : ''
+    return (
+      <g>
+        <text
+          x={x - 15}
+          y={y}
+          fontSize={12}
+          fontWeight="bold"
+          fill="white"
+          fontFamily="'Roboto Condensed', 'Gill Sans', 'Gill Sans MT', 'Ser­avek', 'Trebuchet MS', sans-serif"
+        >
+          {cat}
+        </text>
+        <VictoryTooltip
+          {...this.props}
+          text={`${this.props.text}\n${this.props.datum.y}`}
+          orientation="top"
+          flyoutStyle={{ fill: '#c33f38' }}
+        />
+      </g>
+    )
+  }
+}
 
 const initialState = {
   loading: true,
   roomEmotions: [],
-};
+}
 
 function reducer(state, action) {
   switch (action.type) {
-    case "SET_ROOM_EMOTIONS":
+    case 'SET_ROOM_EMOTIONS':
       return {
         ...state,
         roomEmotions: action.roomEmotions,
         loading: false,
-      };
-    case "SET_LOADING":
+      }
+    case 'SET_LOADING':
       return {
         ...state,
         loading: action.loading,
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
 
+const EmptyMessage = (props) => {
+  if (props.check.length === 0) {
+    return (
+      <div className="roomEmpty">
+        <div className="bb">
+          <h1>This Room is Empty</h1>
+        </div>
+      </div>
+    )
+  }
+  return null
+}
+
 const getFaceStats = (emotions) => {
-  const emoteArray = emotions.map((em) => em.emotion.split(","));
+  const emoteArray = emotions.map((em) => em.emotion.split(','))
   const fCount = emoteArray.reduce(
     (prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev),
     {
@@ -41,34 +92,33 @@ const getFaceStats = (emotions) => {
       disgusted: 0,
       surprised: 0,
     }
-  );
+  )
 
   const chart = [
-    { x: "Happy", y: fCount.happy },
-    { x: "Neutral", y: fCount.neutral },
-    { x: "Surprised", y: fCount.surprised },
-    { x: "Sad", y: fCount.sad },
-    { x: "Fearful", y: fCount.fearful },
-    { x: "Disgusted", y: fCount.disgusted },
-    { x: "Angry", y: fCount.angry },
-  ];
-  const good = fCount.happy + fCount.neutral + fCount.surprised;
-  const bad = fCount.sad + fCount.fearful + fCount.angry + fCount.disgusted;
-  return { chart, good, bad, total: emotions.length };
-};
+    { x: 'Happy', y: fCount.happy },
+    { x: 'Neutral', y: fCount.neutral },
+    { x: 'Surprised', y: fCount.surprised },
+    { x: 'Sad', y: fCount.sad },
+    { x: 'Fearful', y: fCount.fearful },
+    { x: 'Disgusted', y: fCount.disgusted },
+    { x: 'Angry', y: fCount.angry },
+  ]
+
+  return { chart, total: emotions.length }
+}
 
 function WatchRoom() {
-  let params = useParams();
-  const [state, dispatch] = useReducer(reducer, initialState);
-  let subscription;
+  let params = useParams()
+  const [state, dispatch] = useReducer(reducer, initialState)
+  let subscription
 
   useEffect(() => {
-    fetchRoomEmotions();
-    subscribe();
+    fetchRoomEmotions()
+    subscribe()
     return () => {
-      subscription && subscription.unsubscribe();
-    };
-  }, []);
+      subscription && subscription.unsubscribe()
+    }
+  }, [])
 
   async function fetchRoomEmotions() {
     let {
@@ -78,9 +128,9 @@ function WatchRoom() {
     } = await API.graphql({
       query: itemsByRoomName,
       variables: { roomName: params.id },
-    });
+    })
     // console.log("roomItems", roomItems)
-    dispatch({ type: "SET_ROOM_EMOTIONS", roomEmotions: roomItems });
+    dispatch({ type: 'SET_ROOM_EMOTIONS', roomEmotions: roomItems })
   }
 
   function subscribe() {
@@ -89,9 +139,9 @@ function WatchRoom() {
       variables: { id: params.id },
     }).subscribe({
       next: (apiData) => {
-        fetchRoomEmotions();
+        fetchRoomEmotions()
       },
-    });
+    })
   }
 
   return (
@@ -109,39 +159,50 @@ function WatchRoom() {
         </h2>
       </section>
       <section className="resultFramer">
+        <EmptyMessage check={state.roomEmotions} />
         <VictoryPie
           animate={{
             duration: 200,
           }}
-          style={{ labels: { fontSize: 20, fontWeight: "bold" } }}
-          labelRadius={({ innerRadius }) => innerRadius + 50}
-          labelComponent={<VictoryTooltip />}
+          style={{
+            labels: { fontWeight: 'bold', fontSize: 8, fill: 'white' },
+          }}
+          labelRadius={({ innerRadius }) => innerRadius + 100}
+          labelComponent={<CustomLabel />}
           colorScale={[
-            "#029832",
-            "#62b32b",
-            "#C7EA46",
-            "#fedb00",
-            "#f97a00",
-            "#ff5349",
-            "#d50218",
+            '#295796',
+            '#864074',
+            '#a85eb3',
+            '#752fb4',
+            '#422eaf',
+            '#637dc2',
+            '#6b1d1e',
           ]}
           data={getFaceStats(state.roomEmotions).chart}
+          // data={demoChart}
         />
       </section>
+      <h3 class="roomReport">
+        There are {state.roomEmotions.length} people in the room
+      </h3>
       <hr />
       <section id="explainerSection">
-        <h2>What is this page?</h2>
+        <h2>What is this?</h2>
         <img src="/binoc.svg" id="explainWatch" />
         <p>
-          This is the watching area for this room: <a href="">fdsafsd</a>
+          This page is watching the audience members in room{' '}
+          <a href={`/room/${params.id}`}>{params.id}</a>. The emotions of the
+          people in that room will be sent here in real-time. Share the room URL
+          with your audience members.
         </p>
+        <h2>{window.location.href.replace('watch', 'room')}</h2>
         <p>
           If you'd like to create your own room or understand more about this
           project, <a href="/">visit the main page</a>.
         </p>
       </section>
     </div>
-  );
+  )
 }
 
-export default WatchRoom;
+export default WatchRoom
